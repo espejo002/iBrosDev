483793bedf991935e22e2b1abb922633e65f8b9e
espejo002 <iseyemijoshua002@gmail.com>
2023-11-02 18:03:49 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const button = document.querySelector('#button')
+function console(){
+  console.log('We are the coalition of frontend web developers known as iBros Corporations.)
+  console.log('We specialise in HTML and CSS programming. Though if you want, we can add a pinch of JavaScript to your site.')
+  console.log('Thank you for using our services')
+}
+
+console()
