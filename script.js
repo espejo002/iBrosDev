f1cdaa3d4bfdbcb05980aa60f17bc5f9c9ff6224
espejo002 <iseyemijoshua002@gmail.com>
2023-11-02 18:05:16 +0100

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function console(){
-  console.log('We are the coalition of frontend web developers known as iBros Corporations.)
+  console.log('We are the coalition of frontend web developers known as iBros Corporations.')
   console.log('We specialise in HTML and CSS programming. Though if you want, we can add a pinch of JavaScript to your site.')
-  console.log('Thank you for using our services')
+  console.log('Thank you for using our services.😁')
 }
 
 console()
